775f080bbd29a41da5c6dfedf1ec1dbc933dbcda
VSUDEV <dabifrmdao@gmail.com>
2026-01-02 20:18:45 -0500

    Create build.lua

---
+++ build.lua
@@ -0,0 +1,15 @@
+local Build_Info = {}
+
+function Build_Info:VersionType()
+    return '1.0.0'
+end
+
+function Build_Info:GetBuild()
+    return 'Jan 02 2026'
+end
+
+function Build_Info:GetBuildType()
+    return 'Private'
+end
+
+return Build_Info
